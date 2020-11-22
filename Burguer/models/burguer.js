b3d1b63d7fc7a30ca83ccd702235dcaae898d209
unknown <luis.canahuate16@gmail.com>
2020-11-22 18:40:20 -0500

    sytax error fixed on models/ burguer file

--- Burguer/models/burguer.js
+++ Burguer/models/burguer.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burguer = {
   all: function(cb) {
     orm.all("burguer", function(res) {
       cb(res);
