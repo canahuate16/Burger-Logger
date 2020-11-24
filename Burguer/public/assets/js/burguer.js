df20a66479003a3f4360aa5ecb6ab28d77e58c57
unknown <luis.canahuate16@gmail.com>
2020-11-23 22:21:05 -0500

    added public folder and files. Added initial burguer.js code

---
+++ Burguer/public/assets/js/burguer.js
@@ -0,0 +1,48 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devour").on("click", function(event) {
+      const id = $(this).data("id");
+      const status = $(this).data("status");
+  
+      const newStatus = {
+        devoured: newStatus //true?
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burguers/" + id, {
+        type: "PUT",
+        data: newStatus
+      }).then(
+        function() {
+          console.log("changed burguer status to", status);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      const newBurguer = {
+        burguer_name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burguers", {
+        type: "POST",
+        data: newBurguer
+      }).then(
+        function() {
+          console.log("add new burguer");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+
+  });
+  
