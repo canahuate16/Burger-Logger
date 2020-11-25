c46a46c7f3181aee60265a0922216b51732e0e45
unknown <luis.canahuate16@gmail.com>
2020-11-25 18:37:30 -0500

    updating data base

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,21 @@ const mysql = require("mysql");
 require ('dotenv').config()
 
 const connection = mysql.createConnection({
-  host: "j7t5ebkr1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	mlexozlj3",
+  host: "r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "j7t5ebkmlexozlj3",
   password: "lgccyvovnw65b6t2",
   database: "kcbswea71ah0ggw3"
 });
+
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Etauhanac16",
+//   database: "burger_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burger (burger_name) VALUES ('Cheeseburguer');
-INSERT INTO burger (burger_name) VALUES ('Veggie Burguer');
-INSERT INTO burger (burger_name, devoured) VALUES ('Cheeseburguer', false);
-INSERT INTO burger (burger_name, devoured) VALUES ('Veggie Burguer', false);
-INSERT INTO burger (burger_name, devoured) VALUES ('Big Mac', false);
-INSERT INTO burger (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburguer');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burguer');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburguer', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burguer', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
