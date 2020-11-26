7ce728270b4f481281d9ec6d29802836e4839a60
unknown <luis.canahuate16@gmail.com>
2020-11-25 19:43:28 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,27 @@
 const mysql = require("mysql");
 require ('dotenv').config()
 
-const connection = mysql.createConnection({
-  host: "r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "j7t5ebkmlexozlj3",
-  password: "lgccyvovnw65b6t2",
-  database: "kcbswea71ah0ggw3"
-});
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "Etauhanac16",
+      database: "burger_db"
+    });
+}
+
+// const connection = mysql.createConnection({
+//   host: "r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "j7t5ebkmlexozlj3",
+//   password: "lgccyvovnw65b6t2",
+//   database: "kcbswea71ah0ggw3"
+// });
+
 
 
 // var connection = mysql.createConnection({
